bfa8c95883875c2eb8a23a43ebb24b26f539d4b4
Haoyu99Gao <2776344039@qq.com>
2024-04-17 17:58:49 -0400

    ghy

--- model/export_model.py
+++ model/export_model.py
@@ -1,5 +1,5 @@
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import sklearn
@@ -9,74 +9,101 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.preprocessing import LabelEncoder
 
+
 # Read data from CSV
 def read_data(file_path):
-    df=pd.read_csv(file_path)
+    df = pd.read_csv(file_path)
     columns_to_keep = [
-        'HeartDisease','BMI', 'Smoking', 'Stroke', 'PhysicalHealth',
+        'HeartDisease', 'BMI', 'Smoking', 'Stroke', 'PhysicalHealth',
         'DiffWalking', 'Sex', 'AgeCategory', 'Diabetic',
         'Asthma', 'KidneyDisease']
     df = df[columns_to_keep]
     df1 = df.copy()
-    le=LabelEncoder()
-    categorical = df.select_dtypes(include = 'O')
+    le = LabelEncoder()
+    categorical = df.select_dtypes(include='O')
     for feature in categorical:
-        df1[feature]=le.fit_transform(df1[feature])
+        df1[feature] = le.fit_transform(df1[feature])
     return df1
+
 
 # Split the data into training set and test test
 def split_data(X, Y, df):
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2,stratify=df['HeartDisease'])
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, stratify=df['HeartDisease'])
     return X_train, X_test, y_train, y_test
 
+
 # Oversample the training data
-def over_sample_date(X_train, y_train, data_rate = 1):
-    minority_class = y_train.value_counts().idxmin()    
-    majority_class = y_train.value_counts().idxmax()    
+def over_sample_date(X_train, y_train, data_rate=1):
+    minority_class = y_train.value_counts().idxmin()
+    majority_class = y_train.value_counts().idxmax()
 
     minority_data = X_train[y_train == minority_class]
     majority_data = X_train[y_train == majority_class]
     minority_labels = y_train[y_train == minority_class]
     majority_labels = y_train[y_train == majority_class]
 
-    oversampled_minority_data = minority_data.sample(int(len(majority_data)*data_rate), replace=True)
-    oversampled_minority_labels = minority_labels.sample(int(len(majority_data)*data_rate), replace=True)
+    oversampled_minority_data = minority_data.sample(int(len(majority_data) * data_rate), replace=True)
+    oversampled_minority_labels = minority_labels.sample(int(len(majority_data) * data_rate), replace=True)
     X_train_oversampled = pd.concat([majority_data, oversampled_minority_data])
     y_train_oversampled = pd.concat([majority_labels, oversampled_minority_labels])
     return X_train_oversampled, y_train_oversampled
+
 
 # Normalize the row to the training pattern
 def normalize_row(row, scaler):
     row_reshaped = row.values.reshape(1, -1)
     normalized_row = scaler.transform(row_reshaped)
-    return normalized_row  
+    return normalized_row
+
 
 # Normalize the input and compute the Probability 
 def compute_P(input, scaler):
-    user_input_T = normalize_row(pd.DataFrame([input]),scaler)
+    user_input_T = normalize_row(pd.DataFrame([input]), scaler)
     coef = np.array([0.09250334, 0.23067485, 0.30712092, 0.24308795, 0.19264143,
-            0.35994048, 1.03353847, 0.2447468 , 0.13934069, 0.17743164]).flatten()
+                     0.35994048, 1.03353847, 0.2447468, 0.13934069, 0.17743164]).flatten()
     intercept = -1.29850855
     # Compute the linear combination
     z = np.dot(user_input_T, coef) + intercept
     # Apply the logistic regression
     p = 1 / (1 + np.exp(-z))
     return p
-    
-def run_model(user_input = [29.76, 0.0, 0.0, 0.0, 0.0, 1.0, 7.0, 0.0, 0.0, 0.0]):
-    file_path = 'data/data2020.csv'
+
+
+def run_model(user_input=[29.76, 'No', 'No', 'Uncomfortable', 'No', 'No', 7.0, 'No', 'Bad', 'No']):
+    user_input = preprocess_input((user_input))
+    file_path = '../data/data2020.csv'
     df1 = read_data(file_path)
     # Get training data
-    X=df1.drop('HeartDisease',axis=1)
-    Y=df1['HeartDisease']
-    X_train, X_test, y_train, y_test = split_data(X,Y, df1)
-    X_train_oversampled, Y_train_oversampled = over_sample_date(X_train, y_train, data_rate = 0.4)
+    X = df1.drop('HeartDisease', axis=1)
+    Y = df1['HeartDisease']
+    X_train, X_test, y_train, y_test = split_data(X, Y, df1)
+    X_train_oversampled, Y_train_oversampled = over_sample_date(X_train, y_train, data_rate=0.4)
     # Get user input and fit scaler
     # user_input = [29.76, 0.0, 0.0, 0.0, 0.0, 1.0, 7.0, 0.0, 0.0, 0.0]
     scaler = StandardScaler().fit(X_train_oversampled.values)
     # compute the output
     P = compute_P(user_input, scaler)
-    # print(P)
+    print(P)
+    print('ok')
     return P
 
+
+def preprocess_input(user_input):
+    mapping = {
+        'Yes': 1.0,
+        'No': 0.0,
+        'Uncomfortable': 0.0,
+        'Comfortable': 1.0,
+        'Bad': 0.0,
+        'Good': 1.0
+    }
+
+    converted_input = []
+    for value in user_input:
+        if isinstance(value, str) and value in mapping:
+            converted_input.append(mapping[value])
+        else:
+            converted_input.append(value)
+
+    return converted_input
 
--- server.py
+++ server.py
@@ -5,6 +5,7 @@ from model.export_model import run_model
 
 PORT = 8001
 
+
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200, "ok")
@@ -25,12 +26,14 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
             self.test()
         else:
             self.do_GET()
+
     def do_POST(self):
         # print("post headers:")
         # print(self.headers)
         content_length = int(self.headers['Content-Length'])  # Get the size of data
         post_data = self.rfile.read(content_length)  # Gets the data itself
         self.handle_request(post_data.decode('utf-8'))  # Decode it to string
+
     def handle_request(self, input_data):
         try:
             data = json.loads(input_data)  # Try to parse the data as JSON
@@ -42,8 +45,7 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
             p = run_model(data)
             # p = 0.01
             result = {'result': data, 'prob': p[0]}  # Process the data here
-            
-        
+
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')  # Handle CORS
         self.end_headers()
@@ -51,12 +53,13 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
 
     def test(self):
         # Example function 
-        result = "test"  
+        result = "test"
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')  # Handle CORS
         self.end_headers()
         self.wfile.write(json.dumps({'result': result}).encode())
+
 
 httpd = socketserver.TCPServer(("", PORT), CustomHandler)
 print("serving at port", PORT)

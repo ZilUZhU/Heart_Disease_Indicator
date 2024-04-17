1d22f0dc91c7165f9183f5fa84986aa7fa2e3490
Terry Wang <jwang3030@gatech.edu>
2024-04-17 11:18:09 -0400

    Add visual elements and Questionnaire Mechanics

--- visualization/mapView.js
+++ visualization/mapView.js
@@ -1,10 +1,11 @@
 // Dimensions and margins for the map
-var margin = {top: 50, right: 50, bottom: 50, left: 80},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var margin = {top: 50, right: 50, bottom: 50, left: 80};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
-// Append the SVG object to the body of the page
-var svg = d3.select("body").append("svg")
+// Append the SVG object to the #mapContainer
+var svg = d3.select("#mapContainer")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -59,30 +60,25 @@ Promise.all([
 
           if (d[field].startsWith(keyword)) resultRate[d['State']].yes += 1;
         });
-        var rate = 1;
-        // console.log(resultRate[state])
-  
+
         // Calculate percentages
         Object.keys(resultRate).forEach(function(State) {
-            rate = resultRate[State];
+            var rate = resultRate[State];
             rate.percentage = (rate.yes / rate.total) * 100;
         });
-        // console.log(rate)
-        
+
         return resultRate[state];
     }
-
 
     const groupedData = d3.group(healthData, d => d.State);
 
     const averageValueByState = Array.from(groupedData, ([state, values]) => {
       const avgSleepHours = d3.mean(values, d => d.SleepHours);
-      return { State: state, 
-              AverageSleepHours: avgSleepHours, 
+      return { State: state,
+              AverageSleepHours: avgSleepHours,
               AverageBMI: d3.mean(values, d => d.BMI)
              };
     });
-
 
     // Define color scale
     var colorScale = d3.scaleSequential(d3.interpolateReds)
@@ -104,7 +100,7 @@ Promise.all([
             tooltip.transition()
                 .duration(200)
                 .style("opacity", .9);
-            // variables for tootip
+
             var state = d.properties.name;
             var rate = heartAttackRates[state];
             var stateVal = averageValueByState.find(item => item.State === state)
@@ -114,17 +110,15 @@ Promise.all([
             var diabetesRate = calculateRate(healthData, "HadDiabetes", state)
             var smokingRate = calculateRate(healthData, "SmokerStatus", state, 'C')
             var asthmaRate = calculateRate(healthData, "HadAsthma", state)
-            // tooltip text
+
             var text = "<b>" + state + "</b><br>" + (rate ? "Heart Attack Rate: " + rate.percentage.toFixed(2) + "%" : "No data")
-                        + (d3.select('#SleepHours').property('checked') ? "<br>Average Sleep Hours: " + sleepHour: "")  // add checkbox options
-                        + (d3.select('#BMI').property('checked') ? "<br>Average BMI: " + avgBMI: "") 
-                        + (d3.select('#HadStroke').property('checked') ? "<br> Stroke Rate: " + strokeRate.percentage.toFixed(2) + "%": "") 
+                        + (d3.select('#SleepHours').property('checked') ? "<br>Average Sleep Hours: " + sleepHour: "")
+                        + (d3.select('#BMI').property('checked') ? "<br>Average BMI: " + avgBMI: "")
+                        + (d3.select('#HadStroke').property('checked') ? "<br> Stroke Rate: " + strokeRate.percentage.toFixed(2) + "%": "")
                         + (d3.select('#HadDiabetes').property('checked') ? "<br> Diabetes Rate: " + diabetesRate.percentage.toFixed(2) + "%": "")
                         + (d3.select('#SmokerStatus').property('checked') ? "<br> Smoking Rate: " + smokingRate.percentage.toFixed(2) + "%": "")
-                        + (d3.select('#HadAsthma').property('checked') ? "<br> Asthma Rate: " + asthmaRate.percentage.toFixed(2) + "%": "")
-                        ;
-            
-            
+                        + (d3.select('#HadAsthma').property('checked') ? "<br> Asthma Rate: " + asthmaRate.percentage.toFixed(2) + "%": "");
+
             tooltip.html(text)
                 .style("left", (d3.event.pageX) + "px")
                 .style("top", (d3.event.pageY - 28) + "px");
@@ -137,4 +131,3 @@ Promise.all([
 }).catch(function(error) {
     console.error("Error loading or processing data:", error);
 });
-

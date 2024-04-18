95ed232707d6169c31b9b1154c2f07f288afd72a
Pb314314 <Bo.19@intl.zju.edu.cn>
2024-04-18 19:54:18 -0400

    retrain -3

--- model/model_inference.py
+++ model/model_inference.py
@@ -3,20 +3,13 @@ import pickle
 import pandas as pd 
 from joblib import load
 
-# Normalize the row to the training pattern
 def normalize_row(row, scaler):
     row_reshaped = row.values.reshape(1, -1)
     normalized_row = scaler.transform(row_reshaped)
     return normalized_row  
 
-# Normalize the input and compute the Probability 
-# Load the model back from the file
-
-# Use loaded_model to predict or evaluate to ensure it works
-
-def compute_P(input, scaler):
-    loaded_model = load('logistic_model.joblib')
-    
+def compute_P(input, scaler, path):
+    loaded_model = load(path)
     user_input_T = normalize_row(pd.DataFrame([input]),scaler)
     predictions = loaded_model.predict_proba(user_input_T)
     return predictions[0][1]
@@ -24,13 +17,15 @@ def compute_P(input, scaler):
 def main():
     # Get user input and fit scaler
     #user_input = [29.76, 'No', 'No', 'Uncomfortable', 'No', 'No', 7.0, 'No', 'Bad', 'No']
-    with open('scaler.pkl', 'rb') as f:
-        scaler = pickle.load(f)
-    # Transfer user input to number
+    
     user_input = [0.0, 9.0, 4.0, 1.0, 0.0, 3.0, 0.0, 111.13, 1.63, 0.0]
 
+    with open('./saved_files/scaler.pkl', 'rb') as f:
+        scaler = pickle.load(f)
+    
+    model_path = './saved_files/logistic_model.joblib'
     # compute the output
-    P = compute_P(user_input, scaler)
+    P = compute_P(user_input, scaler,model_path)
     print(P)
 
 if __name__ == "__main__":
